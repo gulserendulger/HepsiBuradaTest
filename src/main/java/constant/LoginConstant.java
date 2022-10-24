e14cfff9fe23316c408ef99c27006341bb52949e
Gülseren Dülger <83505989+gulserendulger@users.noreply.github.com>
2022-10-24 09:34:11 +0300

    Initial commit

--- src/main/java/constant/LoginConstant.java
+++ src/main/java/constant/LoginConstant.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 
 public class LoginConstant {
 
-    public static final String  EMAIL="gulserentestinium@gmail.com";
-    public static final String  PASSWORD="Test1234.";
+    public static final String EMAIL = "";
+    public static final String PASSWORD = "";
 
     public static final By ACCOUNTTEXT_CONTROL = By.cssSelector("input[id=\"txtUserName\"]");
     public static final By LOGIN_BUTTON = By.cssSelector("button[id=\"btnLogin\"]");
@@ -13,6 +13,6 @@ public class LoginConstant {
     public static final By GOOGLE_EMAIL = By.cssSelector("input[id=\"identifierId\"]");
     public static final By GOOGLE_EMAIL_NEXT = By.cssSelector("div[id=\"identifierNext\"]");
     public static final By GOOGLE_PASSWORD = By.cssSelector("input[type=\"password\"]");
-    public static final By GOOGLE_PASSWORD_NEXT= By.cssSelector("div[id=\"passwordNext\"]");
+    public static final By GOOGLE_PASSWORD_NEXT = By.cssSelector("div[id=\"passwordNext\"]");
 
 }
